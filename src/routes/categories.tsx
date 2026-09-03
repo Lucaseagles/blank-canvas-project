0a2102954ce92df3d9bd325e79acc16550ce33b4
Lucaseagles <105374052+Lucaseagles@users.noreply.github.com>
2026-09-03 19:41:46 -0300

    feat(sprint37): add categories route

---
+++ src/routes/categories.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { CategoriesPage } from "@/categories/CategoriesPage";
+export const Route = createFileRoute("/categories")({ component: CategoriesPage });
